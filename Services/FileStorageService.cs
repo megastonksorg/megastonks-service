541481146ca0e2c588797ad4ad7c08dfd77e8fda
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2022-07-25 21:17:13 -0700

    Modify location of azure blob connection string Add FileStorage service to help with uploading images

---
+++ Services/FileStorageService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using Azure.Storage.Blobs;
+using Megastonks.Helpers;
+
+namespace Megastonks.Services
+{
+    public class FileStorageService
+    {
+        private readonly IConfiguration Configuration;
+
+        public FileStorageService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public Uri UploadImage(byte [] imageFile)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(Configuration.GetConnectionString("AzureBlobStorage"));
+            BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient("images");
+
+            string fileName = $"{Guid.NewGuid()}.png";
+            BlobClient blobClient = blobContainerClient.GetBlobClient(fileName);
+            using var memoryStream = new MemoryStream(imageFile, false);
+            if (memoryStream != null) {
+                blobClient.Upload(memoryStream);
+            }
+
+            return blobClient.Uri;
+        }
+    }
+}
