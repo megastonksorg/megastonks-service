a2ea53e4d6d0f1d8249c75b9a7a0d7f2924a5ee5
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-20 21:00:30 -0800

    Add TribeService and it's interface

---
+++ Services/TribeService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Megastonks.Helpers;
+using Megastonks.Models.Tribe;
+
+namespace Megastonks.Services
+{
+    public interface ITribeService
+    {
+        TribeResponse CreateTribe(string name);
+    }
+
+    public class TribeService : ITribeService
+    {
+        private readonly ILogger<AccountService> _logger;
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public TribeService(ILogger<AccountService> logger, DataContext context, IMapper mapper)
+        {
+            _logger = logger;
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public TribeResponse CreateTribe(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
