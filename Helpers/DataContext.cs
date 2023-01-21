25c58eb783e881817b8aa9b0fed3561151f4662b
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-20 19:06:43 -0800

    Format empty space in DataContext and make the code in TribeInviteCode unique in the builder

--- Helpers/DataContext.cs
+++ Helpers/DataContext.cs
@@ -3,28 +3,32 @@ using Megastonks.Entities;
 
 namespace Megastonks.Helpers
 {
-	public class DataContext : DbContext
-	{
-		public DbSet<Account> Accounts { get; set; }
+    public class DataContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
 
-		private readonly IConfiguration Configuration;
+        private readonly IConfiguration Configuration;
 
-		public DataContext(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		protected override void OnConfiguring(DbContextOptionsBuilder options)
-		{
-			// connect to SqlServer database
-			options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
-		}
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to SqlServer database
+            options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+        }
 
-		protected override void OnModelCreating(ModelBuilder builder)
-		{
-			builder.Entity<Account>()
-				.HasIndex(x => new { x.WalletAddress })
-				.IsUnique();
-		}
-	}
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Account>()
+                .HasIndex(x => new { x.WalletAddress })
+                .IsUnique();
+
+            builder.Entity<TribeInviteCode>()
+                .HasIndex(inviteCode => inviteCode.Code)
+                .IsUnique();
+        }
+    }
 }
