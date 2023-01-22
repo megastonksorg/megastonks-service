9d768df679910cecc33583044018ebedb9793c77
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-01-21 20:25:17 -0800

    Add TribeController with the create call

---
+++ Controllers/TribeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Megastonks.Models.Tribe;
+using Megastonks.Services;
+
+namespace Megastonks.Controllers
+{
+	[Authorize]
+	[Controller]
+	[Route("[controller]")]
+	public class TribeController : BaseController
+	{
+        private readonly TribeService _tribeService;
+
+        public TribeController(TribeService tribeService)
+		{
+			_tribeService = tribeService;
+		}
+
+        [HttpPost("create")]
+        public ActionResult<TribeResponse> CreateTribe(string name)
+        {
+            var result = _tribeService.CreateTribe(Account, name);
+            return Ok(result);
+        }
+    }
+}
