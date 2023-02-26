b42e4bdf83b117efc17dae766ba351b3eccd8d98
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-26 15:40:05 -0800

    Add the base class for the MessageService

---
+++ Services/MessageService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AutoMapper;
+using Megastonks.Helpers;
+
+namespace Megastonks.Services
+{
+    public interface IMessageService
+    {
+
+    }
+
+    public class MessageService : IMessageService
+    {
+        private readonly ILogger<MessageService> _logger;
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public MessageService(ILogger<MessageService> logger, DataContext context, IMapper mapper)
+        {
+            _logger = logger;
+            _context = context;
+            _mapper = mapper;
+        }
+    }
+}
