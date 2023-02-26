cfc99b80ef5fba04e7be3b599a46e74d783bbeb1
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-26 15:39:46 -0800

    Add message folder to Models and add the MessagePostRequest

---
+++ Models/Message/MessagePostRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Megastonks.Models.Message
+{
+    public class MessagePostRequest
+    {
+        public class Key
+        {
+            public string PublicKey { get; set; }
+            public string EncryptionKey { get; set; }
+        }
+
+        public class MessageContent
+        {
+            public string Body { get; set; }
+            public string Caption { get; set; }
+            public string Type { get; set; }
+        }
+
+        public MessageContent Content { get; set; }
+        public string ContextId { get; set; }
+        public List<Key> Keys { get; set; }
+    }
+}
