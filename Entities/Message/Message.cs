ae0863c012bf558f28fdcdf199603e899e8610f5
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-26 14:09:57 -0800

    Add Tribe to message

--- Entities/Message/Message.cs
+++ Entities/Message/Message.cs
@@ -7,6 +7,7 @@ namespace Megastonks.Entities.Message
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
+        public Tribe Tribe { get; set; }
         public MessageContent Content { get; set; } 
         public Message? Context { get; set; } 
         public Account? Sender { get; set; }
