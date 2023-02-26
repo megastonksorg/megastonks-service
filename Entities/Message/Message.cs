d11f2b96c8a65c45783438920837a8d0b23bd943
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-26 12:00:44 -0800

    Group entities into Account, Tribe and Message categories. Then add Message file

---
+++ Entities/Message/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Megastonks.Entities
+{
+    public class Message
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+    }
+}
