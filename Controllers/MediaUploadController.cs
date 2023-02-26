0f3480d0e2d491c5930e6d82e364724e47dda632
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-25 20:55:41 -0800

    Add video file and video data upload to the media upload controller

--- Controllers/MediaUploadController.cs
+++ Controllers/MediaUploadController.cs
@@ -28,5 +28,21 @@ namespace Megastonks.Controllers
             var response = _mediaUploadService.UploadImage(imageData);
             return Ok(response);
         }
+
+        [HttpPut("videoFile")]
+        [RequestSizeLimit(bytes: 150_000_000)] //150MB
+        public ActionResult<Uri> Video()
+        {
+            IFormFile file = Request.Form.Files.FirstOrDefault();
+            var response = _mediaUploadService.UploadVideoFile(file);
+            return Ok(response);
+        }
+
+        [HttpPut("video")]
+        public ActionResult<Uri> Video([FromBody] byte[] videoData)
+        {
+            var response = _mediaUploadService.UploadVideo(videoData);
+            return Ok(response);
+        }
     }
 }
