7495d705d51512c346355347289de411b9859746
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-26 16:47:45 -0800

    Add MessageContent to models then add MessageResponse

---
+++ Models/Message/MessageContent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Megastonks.Models.Message
+{
+    public class MessageContent
+    {
+        public string Body { get; set; }
+        public string Caption { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ Models/Message/MessageResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Megastonks.Models.Message
+{
+    public class MessageResponse
+    {
+        public class Key
+        {
+            public string PublicKey { get; set; }
+            public string EncryptionKey { get; set; }
+        }
+
+        public class Reaction
+        {
+            public string SenderWalletAddress { get; set; }
+            public string Content { get; set; }
+        }
+
+        public string Id { get; set; }
+        public MessageContent Content { get; set; }
+        public MessageResponse? Context { get; set; }
+        public string? SenderWalletAddress { get; set; }
+        public string Tag { get; set; }
+        public List<Key> Keys { get; set; }
+        public List<Reaction>? Reactions { get; set; }
+        public DateTime? Expires { get; set; }
+        public DateTime TimeStamp { get; set; } 
+    }
+}
