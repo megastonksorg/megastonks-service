3aca8388cf23fbb9d1855d784afb00575694d377
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-02-27 12:52:33 -0800

    Add The MessageController and add a method for posting tea

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Megastonks.Models.Account;
+using Megastonks.Models.Message;
+using Megastonks.Models.Tribe;
+using Megastonks.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Megastonks.Controllers
+{
+    [Authorize]
+    [Controller]
+    [Route("[controller]")]
+    public class MessageController : BaseController
+	{
+        private readonly IMessageService _messageService;
+
+        public MessageController(IMessageService messageService)
+		{
+			_messageService = messageService;
+		}
+
+        [HttpPost("tea")]
+        public ActionResult<MessageResponse> PostMessage([FromBody] PostMessageReqest model)
+        {
+            var result = _messageService.PostMessage(Account, Entities.Message.MessageTag.tea, model);
+            return Ok(result);
+        }
+    }
+}
