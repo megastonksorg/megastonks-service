5b75a2fed2c54f0840fe3d21437a960a7d8d975f
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-16 10:46:26 -0700

    Fix empty space formatting in the BaseController

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -3,9 +3,9 @@ using Megastonks.Entities;
 
 namespace Megastonks.Controllers
 {
-	[Controller]
-	public abstract class BaseController : ControllerBase
-	{
+    [Controller]
+    public abstract class BaseController : ControllerBase
+    {
         // returns the current authenticated account (null if not logged in)
         public Account Account => (Account)HttpContext.Items["Account"];
     }
