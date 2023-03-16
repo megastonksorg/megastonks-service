27373dcd7760beac8c076e4c80d2379c089f93ad
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-16 11:31:32 -0700

    Get the current account from the HTTPContext so we could use that to determine if the User can join the Tribe group

--- Hubs/AppHub.cs
+++ Hubs/AppHub.cs
@@ -1,13 +1,41 @@
 ﻿using System;
+using Megastonks.Entities;
+using Megastonks.Helpers;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Megastonks.Hubs
 {
     public class AppHub : Hub
     {
+        private readonly ILogger<AppHub> _logger;
+        private readonly DataContext _dataContext;
+
+        public AppHub(ILogger<AppHub> logger, DataContext dataContext)
+        {
+            _logger = logger;
+            _dataContext = dataContext;
+        }
+
         public async Task JoinGroup(string tribeId)
         {
-            await Groups.AddToGroupAsync(Context.ConnectionId, tribeId);
+            try
+            {
+                Account account = (Account)Context.GetHttpContext().Items["Account"];
+                var tribe = _dataContext.Tribes.Find(tribeId);
+
+                if (account != null && tribe != null)
+                {
+                    if (tribe.TribeMembers.Find(x => x.Account == account) != null)
+                    {
+                        await Groups.AddToGroupAsync(Context.ConnectionId, tribeId);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                _logger.LogError("Could not Join Group");
+                _logger.LogError(e.StackTrace);
+            }
         }
 
         public async Task LeaveGroup(string tribeId)
