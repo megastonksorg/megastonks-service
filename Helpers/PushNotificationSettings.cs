3aef82c9b2bf3b70fb34ac694f9df0a1037e81df
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-19 21:44:48 -0700

    Add CorePush to the project then update the configuration file for PushNotificationSettings. Then update the PushNotificationService to actually send out a PushNotification

--- Helpers/PushNotificationSettings.cs
+++ Helpers/PushNotificationSettings.cs
@@ -6,12 +6,12 @@ namespace Megastonks.Helpers
         public class AppleSettings
         {
             public string P8Key { get; set; }
-            public string PrivateKey { get; set; }
+            public string P8KeyId { get; set; }
             public string TeamId { get; set; }
             public string AppIdentifier { get; set; }
-            public string Server { get; set; }
         }
 
         public AppleSettings Apple { get; set; }
+        public string Server { get; set; }
     }
 }
--- Services/PushNotificationService.cs
+++ Services/PushNotificationService.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using Megastonks.Entities;
+﻿using Megastonks.Entities;
+using CorePush;
 using Megastonks.Helpers;
 using Microsoft.Extensions.Options;
+using CorePush.Apple;
+using Newtonsoft.Json;
 
 namespace Megastonks.Services
 {
@@ -12,6 +14,22 @@ namespace Megastonks.Services
 
     public class PushNotificationService : IPushNotificationService
     {
+        public class AppleNotification
+        {
+            public class ApsPayload
+            {
+                [JsonProperty("alert")]
+                public string AlertBody { get; set; }
+
+                [JsonProperty("sound")]
+                public string Sound { get; set; }
+            }
+
+            //Custom properties as needed
+            [JsonProperty("aps")]
+            public ApsPayload Aps { get; set; }
+        }
+
         private readonly HttpClient _client;
         private readonly PushNotificationSettings _settings;
         private readonly ILogger<PushNotificationService> _logger;
@@ -27,7 +45,40 @@ namespace Megastonks.Services
         {
             try
             {
+                DeviceType? deviceType = account.DeviceType;
+                string? deviceToken = account.DeviceToken;
 
+                if (deviceType != null && deviceToken != null)
+                {
+                    switch (deviceType)
+                    {
+                        case DeviceType.apple:
+                            var notification = new AppleNotification
+                            {
+                                Aps = new AppleNotification.ApsPayload
+                                {
+                                    AlertBody = body,
+                                    Sound = "default"
+                                }
+                            };
+
+                            var settings = new ApnSettings
+                            {
+                                AppBundleIdentifier = _settings.Apple.AppIdentifier,
+                                P8PrivateKey = _settings.Apple.P8Key,
+                                P8PrivateKeyId = _settings.Apple.P8KeyId,
+                                TeamId = _settings.Apple.TeamId,
+                                ServerType = _settings.Server == "Production" ? ApnServerType.Production : ApnServerType.Development
+                            };
+
+                            var apn = new ApnSender(settings, _client);
+                            await apn.SendAsync(notification, deviceToken);
+                            return;
+
+                        case DeviceType.android:
+                            return;
+                    }
+                }
             }
             catch (Exception e)
             {
