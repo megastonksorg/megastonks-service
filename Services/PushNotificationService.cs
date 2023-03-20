243c0b1d84afb03598d167df423a30a835416e1f
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-19 20:50:56 -0700

    Add logger to the PushNotification service so we could log any errors

--- Services/PushNotificationService.cs
+++ Services/PushNotificationService.cs
@@ -7,23 +7,32 @@ namespace Megastonks.Services
 {
     public interface IPushNotificationService
     {
-        void SendPush(Account account, string info);
+        void SendPush(Account account, string body);
     }
 
     public class PushNotificationService : IPushNotificationService
     {
         private readonly HttpClient _client;
         private readonly PushNotificationSettings _settings;
+        private readonly ILogger<PushNotificationService> _logger;
 
-        public PushNotificationService(IOptions<PushNotificationSettings> settings)
+        public PushNotificationService(IOptions<PushNotificationSettings> settings, ILogger<PushNotificationService> logger)
         {
             _client = new HttpClient();
-            _settings = settings.Value; 
+            _settings = settings.Value;
+            _logger = logger;
         }
 
-        public async void SendPush(Account account, string info)
+        public async void SendPush(Account account, string body)
         {
+            try
+            {
 
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.StackTrace);
+            }
         }
     }
 }
