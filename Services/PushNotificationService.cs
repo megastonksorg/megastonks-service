0fa1d32beede76516c89b03055c234b2f59c1584
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-19 20:32:18 -0700

    Add PushNotificationService file

---
+++ Services/PushNotificationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Megastonks.Entities;
+using Megastonks.Helpers;
+using Microsoft.Extensions.Options;
+
+namespace Megastonks.Services
+{
+    public interface IPushNotificationService
+    {
+        void SendPush(Account account, string info);
+    }
+
+    public class PushNotificationService : IPushNotificationService
+    {
+        private readonly HttpClient _client;
+        private readonly PushNotificationSettings _settings;
+
+        public PushNotificationService(IOptions<PushNotificationSettings> settings)
+        {
+            _client = new HttpClient();
+            _settings = settings.Value; 
+        }
+
+        public async void SendPush(Account account, string info)
+        {
+
+        }
+    }
+}
