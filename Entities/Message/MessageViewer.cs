822084480cf05e78ca69a2a75ea93ebe36c1d06e
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-22 20:46:50 -0700

    Add MessageViewer entity and the MessageViewer Context

---
+++ Entities/Message/MessageViewer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Megastonks.Entities
+{
+    public class MessageViewer
+    {
+        public int Id { get; set; }
+        public Message Message { get; set; }
+        public List<Account> Viewers { get; set; }
+    }
+}
