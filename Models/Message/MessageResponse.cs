5efdbcc42c0c926cb9e0ed2b640a71261e2aceb4
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-03-24 21:00:09 -0700

    Remove Deleted from the MessageResponse model

--- Models/Message/MessageResponse.cs
+++ Models/Message/MessageResponse.cs
@@ -14,7 +14,6 @@ namespace Megastonks.Models.Message
         public Guid Id { get; set; }
         public string Body { get; set; }
         public string? Caption { get; set; }
-        public bool Deleted { get; set; }
         public MessageType Type { get; set; }
         public string SenderWalletAddress { get; set; }
         public MessageTag Tag { get; set; }
